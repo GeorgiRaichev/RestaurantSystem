2bc420548ba55b37187ddfdf4c936671662e62f1
GeorgiRaichev <88005729+GeorgiRaichev@users.noreply.github.com>
2024-12-27 13:37:15 +0200

    Added function cancel order

--- RestaurantOperatingSystem/order.cpp
+++ RestaurantOperatingSystem/order.cpp
@@ -6,36 +6,74 @@ using namespace std;
 
 // Функция за добавяне на поръчка
 void addOrder() {
-    ifstream menuFile("data/menu.txt");  // Отваряне на менюто
-    ofstream orderFile("data/orders.txt", ios::app);  // Запис на поръчка в края на файла
+	ifstream menuFile("data/menu.txt");  // Отваряне на менюто
+	ofstream orderFile("data/orders.txt", ios::app);  // Запис на поръчка в края на файла
 
-    if (!menuFile) {
-        cout << "Error: Menu file not found.\n";
-        return;
-    }
+	if (!menuFile) {
+		cout << "Error: Menu file not found.\n";
+		return;
+	}
 
-    string itemName;
-    cout << "Enter item name to order: ";
-    cin >> itemName;
+	string itemName;
+	cout << "Enter item name to order: ";
+	cin >> itemName;
 
-    string menuItem;
-    double price;
-    bool itemFound = false;
+	string menuItem;
+	double price;
+	bool itemFound = false;
 
-    // Търсене на артикул в менюто
-    while (menuFile >> menuItem >> price) {
-        if (menuItem == itemName) {
-            itemFound = true;
-            orderFile << itemName << " " << price << " лв." << endl;
-            cout << itemName << " added to orders for " << price << " лв.\n";
-            break;
-        }
-    }
+	// Търсене на артикул в менюто
+	while (menuFile >> menuItem >> price) {
+		if (menuItem == itemName) {
+			itemFound = true;
+			orderFile << itemName << " " << price << " лв." << endl;
+			cout << itemName << " added to orders for " << price << " лв.\n";
+			break;
+		}
+	}
 
-    if (!itemFound) {
-        cout << "Error: " << itemName << " is not available in the menu. Order canceled.\n";
-    }
+	if (!itemFound) {
+		cout << "Error: " << itemName << " is not available in the menu. Order canceled.\n";
+	}
 
-    menuFile.close();
-    orderFile.close();
+	menuFile.close();
+	orderFile.close();
 }
+void cancelOrder() {
+	ifstream orderFile("data/orders.txt");
+	ofstream tempFile("data/temp.txt");
+
+	if (!orderFile) {
+		cout << "Error: Orders file not found.\n";
+		return;
+	}
+
+	string itemName;
+	cout << "Enter item name to cancel: ";
+	cin >> itemName;
+
+	string orderItem;
+	double price;
+	bool orderFound = false;
+
+	// Прехвърляне на поръчките, без тази за изтриване
+	while (orderFile >> orderItem >> price) {
+		if (orderItem == itemName) {
+			cout << itemName << " order canceled successfully.\n";
+			orderFound = true;
+		}
+		else {
+			tempFile << orderItem << " " << price << " лв." << endl;
+		}
+	}
+
+	if (!orderFound) {
+		cout << "Error: Order for " << itemName << " not found.\n";
+	}
+
+	orderFile.close();
+	tempFile.close();
+
+	// Заместване на стария файл с новия
+	remove("data/orders.txt");
+	rename("data/temp.txt", "data/orders.txt");
--- RestaurantOperatingSystem/order.h
+++ RestaurantOperatingSystem/order.h
@@ -1,6 +1,7 @@
 ﻿#ifndef ORDER_H
 #define ORDER_H
 
-void addOrder();  // Функция за добавяне на поръчка
+void addOrder();       // Добавяне на поръчка
+void cancelOrder();    // Отмяна на поръчка
 
 #endif
